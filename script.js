2a5ae5c797de425c84d7bf5feefd1d2a112e1047
prasad-suku <142576212+prasad-suku@users.noreply.github.com>
2024-11-12 08:04:38 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,17 @@
-const url = require("url");
-const fs = require("fs");
-const http = require("http");
+const express = require("express");
+const app = express();
 const path = require("path");
+// Middleware to parse URL-encoded data
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.use(express.urlencoded({ extended: false }));
 
-http
-  .createServer((req, res) => {
-    // Parse the URL to get the path
-    const parsedUrl = url.parse(req.url, true);
-    let pathname = parsedUrl.pathname;
-    console.log(pathname);
+app.post("/survey", (req, res) => {
+  console.log(req.body); // Parsed form data is accessible here
+  console.log("header", req.method);
 
-    // Default to index.html if root path is requested
-    if (pathname === "/") {
-      pathname = "./index.html";
-    }
+  res.send("Survey submitted successfully");
+});
 
-    // Construct the file path
-    // const filePath = path.join(__dirname, pathname);
-
-    // Check if the requested file has .html extension for security
-    // if (path.extname(filePath) !== ".html") {
-    //   res.writeHead(403, { "Content-Type": "text/html" });
-    //   return res.end("<h1>Forbidden</h1>");
-    // }
-
-    // Read and serve the file
-    fs.readFile(pathname, (err, data) => {
-      if (err) {
-        // If the file is not found or there's an error, return 404
-        res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("<h1>404 Not Found</h1>");
-      }
-      // Serve the file content
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-  })
-  .listen(9000, () => {
-    console.log("Server running on http://localhost:9000");
-  });
+app.listen(3000, () => console.log("serving running"));
